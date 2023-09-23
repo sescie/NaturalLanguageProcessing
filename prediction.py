709cec19132f96ccf0aeeca9ebf351dc54b9f985
Perfect-Princess Makuwerere <93464813+naku2001@users.noreply.github.com>
2023-09-23 21:46:52 +0200

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -7,7 +7,7 @@ import pickle
 
 # Load the model and tokenizer
 model = load_model('best_model2.h5',compile=False)
-tokenizer = pickle.load(open('tokenizer (3).pkl', 'rb'))
+tokenizer = pickle.load(open('tokenizerr.pkl', 'rb'))
 
 
 
